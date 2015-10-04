436664d8d8ebf1524f92655262f1cb51edc15c1b
Rahul Madanahalli <rm768@cornell.edu>
2015-10-04 18:21:00 -0400

    Elevators now reschedule floors if they're at maximum capacity
    Fixed sort method for destination queue.
    Created update system that re-assigns a floor if the most optimal
    elevator is full.

--- elevator_saga.js
+++ elevator_saga.js
@@ -2,7 +2,7 @@
     init: function(elevators, floors) {
         floors_going_up = {}; //queue of floors that the elevator will visit next time it is going up.
         floors_going_down = {}; //queue of floors that the elevator will visit the next time it is going down.
-        loadMax = 0.7;
+        loadMax = .62;
         for (var i = 0; i < elevators.length; i++) {
             elevator = elevators[i];
             floors_going_down[elevator] = [];
@@ -13,20 +13,27 @@
             elevator.goingDownIndicator(false);
         }
 
+        function diff(a,b) {
+            return a - b;
+        }
+
         function assign_elevator(floorNum, direction) {
             for (var i = 0; i < elevators.length; i++) {
                 elevator = elevators[i];
                 if (direction == "up") {
                     if (elevator.goingUpIndicator() && elevator.currentFloor() <= floorNum && elevator.loadFactor() < loadMax) {
+                        //window.alert("floor: " + floorNum + " going " + direction + " received elevator " + i);
                         return elevator;
                     }
                 } else {
                     if (elevator.goingDownIndicator() && elevator.currentFloor() >= floorNum && elevator.loadFactor() < loadMax) {
+                        //window.alert("floor: " + floorNum + " going " + direction + " received elevator " + i);
                         return elevator;
                     }
                 }
 
             }
+            //window.alert("floor: " + floorNum + " going " + direction + " received nothing");
             return null;
         }
 
@@ -34,7 +41,7 @@
             if (elevator.goingUpIndicator()) { //if elevator is going up
                 if (floorNum > elevator.currentFloor() && elevator.loadFactor() < loadMax) {//if floor is on the way up and elevator isn't full
                     elevator.destinationQueue.push(floorNum); //add floor to elevator path
-                    elevator.destinationQueue.sort();
+                    elevator.destinationQueue.sort(diff);
                     elevator.checkDestinationQueue();
                 } else {                             //if elevator already passed this floor
                     floors_going_up[elevator].push(floorNum);//queue the elevator for the next time you're going up.
@@ -42,13 +49,13 @@
             } else if (elevator.goingDownIndicator()) {//if elevator is going down
                 floors_going_up[elevator].push(floorNum); //queue the elevator for the next time you're going up.    
             }
-            //window.alert("floor up " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up + "], goingDown: [" + floors_going_down + "]");
+            //window.alert("floor up " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up[elevator] + "], goingDown: [" + floors_going_down[elevator] + "]");
         }
         function down_pressed(elevator, floorNum) {
             if (elevator.goingDownIndicator()) {//if elevator is going down
                 if (floorNum < elevator.currentFloor() && elevator.loadFactor() < loadMax) { //if floor is on the way down and elevator isn't full
                     elevator.destinationQueue.push(floorNum);//add floor to elevator path
-                    elevator.destinationQueue.sort();
+                    elevator.destinationQueue.sort(diff);
                     elevator.destinationQueue.reverse();
                     elevator.checkDestinationQueue();
                 } else {                                             //if elevator already passed this floor 
@@ -57,7 +64,7 @@
             } else if (elevator.goingUpIndicator()) {
                 floors_going_down[elevator].push(floorNum); //queue the elevator for the next time you're going up.
             }
-            //window.alert("floor down " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up + "], goingDown: [" + floors_going_down + "]");
+            //window.alert("floor down " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up[elevator] + "], goingDown: [" + floors_going_down[elevator] + "]");
         }
 
         //button gets pressed on floor 0
@@ -76,7 +83,7 @@
             if (elevator.goingUpIndicator()) { //if elevator is going up
                 if (floorNum >= elevator.currentFloor() && elevator.loadFactor() < loadMax) {//if floor is on the way up and elevator isn't full
                     elevator.destinationQueue.push(floorNum); //add floor to elevator path
-                    elevator.destinationQueue.sort();
+                    elevator.destinationQueue.sort(diff);
                     elevator.checkDestinationQueue();
                     //window.alert("floor down " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up + "], goingDown: [" + floors_going_down + "]");
                 } else {                             //if elevator already passed this floor
@@ -84,7 +91,7 @@
                 }
             } else if (elevator.goingDownIndicator()) {//if elevator is going down
                 elevator.destinationQueue.push(floorNum); //add floor to elevator path because you'll be going towards the bottom floor
-                elevator.destinationQueue.sort();
+                elevator.destinationQueue.sort(diff);
                 elevator.destinationQueue.reverse();
                 elevator.checkDestinationQueue();    
             }
@@ -98,7 +105,7 @@
 
                 for (var i = 0; i < elevators.length; i++) {
                     if (elevators[i].destinationQueue.indexOf(this.floorNum()) != -1 && elevators[i].goingUpIndicator()) {
-                        
+
                         return;
                     }
                 }
@@ -135,7 +142,7 @@
             if (elevator.goingDownIndicator()) {//if elevator is going down
                 if (floorNum <= elevator.currentFloor() && elevator.loadFactor() < loadMax) { //if floor is on the way down and elevator isn't full
                     elevator.destinationQueue.push(floorNum);//add floor to elevator path
-                    elevator.destinationQueue.sort();
+                    elevator.destinationQueue.sort(diff);
                     elevator.destinationQueue.reverse();
                     elevator.checkDestinationQueue();
                 } else {                                             //if elevator already passed this floor 
@@ -143,7 +150,7 @@
                 }
             } else if (elevator.goingUpIndicator()) { //if elevator is going up
                 elevator.destinationQueue.push(floorNum);//add floor to elevator path because elevator will be travelling towards the top floor
-                elevator.destinationQueue.sort();
+                elevator.destinationQueue.sort(diff);
                 elevator.checkDestinationQueue();
             }
             //window.alert("floor down " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up + "], goingDown: [" + floors_going_down + "]");
@@ -157,15 +164,16 @@
                 if (this.goingUpIndicator()) { //if elevator is going up
                     if (floorNum >= this.currentFloor()) {//if floor is on the way up
                         this.destinationQueue.push(floorNum); //add floor to elevator path
-                        this.destinationQueue.sort();
+                        this.destinationQueue.sort(diff);
                         this.checkDestinationQueue();
+                        //window.alert(this.destinationQueue);
                     } else {                             //if elevator already passed this floor
                         floors_going_up[this].push(floorNum);//queue the elevator for the next time you're going up.
                     }
                 } else if (this.goingDownIndicator()) {//if elevator is going down
                     if (floorNum <= this.currentFloor()) { //if floor is on the way down
                         this.destinationQueue.push(floorNum);//add floor to elevator path
-                        this.destinationQueue.sort();
+                        this.destinationQueue.sort(diff);
                         this.destinationQueue.reverse();
                         this.checkDestinationQueue();
                     } else {                                             //if elevator already passed this floor 
@@ -181,14 +189,14 @@
                 if (floorNum == 0) { //if the elevator is on floor 1
                     if (floors_going_up[this].length != 0 && this.destinationQueue.length == 0){ //if the going up queue isn't empty and the destination queue is empty
                         this.destinationQueue = floors_going_up[this]; //make destination queue the going up queue
-                        this.destinationQueue.sort();
+                        this.destinationQueue.sort(diff);
                         floors_going_up[this] = [];   //reset going up queue
-                        while (this.destinationQueue.indexOf(0) != -1) {
-                            this.destinationQueue.splice(this.destinationQueue.indexOf(0), 1);
-                            this.checkDestinationQueue();
-                            //window.alert(this.destinationQueue);
-                        }
                         this.checkDestinationQueue();
+                        //while (this.destinationQueue.indexOf(0) != -1) {
+                        //  this.destinationQueue.splice(this.destinationQueue.indexOf(0), 1);
+                        //  this.checkDestinationQueue();
+                        //window.alert(this.destinationQueue);
+                        //}
                     }
 
                     this.goingUpIndicator(true); 
@@ -197,7 +205,7 @@
                 } else if (floorNum == floors.length - 1) {
                     if (floors_going_down[this].length != 0 && this.destinationQueue.length == 0){ //if the going down queue isn't empty and the destination queue is empty
                         this.destinationQueue = floors_going_down[this]; //make destination queue the going down queue
-                        this.destinationQueue.sort();
+                        this.destinationQueue.sort(diff);
                         this.destinationQueue.reverse();
                         floors_going_down[this] = [];                        //reset going down queue
                         this.checkDestinationQueue();
@@ -211,14 +219,14 @@
                 //window.alert(floors_going_down[this])
                 if (floors_going_down[this].length != 0 && this.destinationQueue.length == 0){ //if the going down queue isn't empty and the destination queue is empty
                     this.destinationQueue = floors_going_down[this]; //make destination queue the going down queue
-                    this.destinationQueue.sort();
+                    this.destinationQueue.sort(diff);
                     this.destinationQueue.reverse();
                     floors_going_down[this] = [];                        //reset going down queue
                     this.checkDestinationQueue();
                 } else {
                     if (this.destinationQueue.indexOf(0) == -1) {
                         this.destinationQueue.push(0);
-                        this.destinationQueue.sort();
+                        this.destinationQueue.sort(diff);
                         this.destinationQueue.reverse();
                         this.checkDestinationQueue();
                         //window.alert(this.destinationQueue);
@@ -234,42 +242,61 @@
 
     },
         update: function(dt, elevators, floors) {
-            /*
+            function up_pressed(elevator, floorNum) {
+                if (elevator.goingUpIndicator()) { //if elevator is going up
+                    if (floorNum > elevator.currentFloor() && elevator.loadFactor() < loadMax) {//if floor is on the way up and elevator isn't full
+                        elevator.destinationQueue.push(floorNum); //add floor to elevator path
+                        elevator.destinationQueue.sort(diff);
+                        elevator.checkDestinationQueue();
+                    } else {                             //if elevator already passed this floor
+                        floors_going_up[elevator].push(floorNum);//queue the elevator for the next time you're going up.
+                    }
+                } else if (elevator.goingDownIndicator()) {//if elevator is going down
+                    floors_going_up[elevator].push(floorNum); //queue the elevator for the next time you're going up.    
+                }
+                //window.alert("floor up " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up[elevator] + "], goingDown: [" + floors_going_down[elevator] + "]");
+            }
+            function down_pressed(elevator, floorNum) {
+                if (elevator.goingDownIndicator()) {//if elevator is going down
+                    if (floorNum < elevator.currentFloor() && elevator.loadFactor() < loadMax) { //if floor is on the way down and elevator isn't full
+                        elevator.destinationQueue.push(floorNum);//add floor to elevator path
+                        elevator.destinationQueue.sort(diff);
+                        elevator.destinationQueue.reverse();
+                        elevator.checkDestinationQueue();
+                    } else {                                             //if elevator already passed this floor 
+                        floors_going_down[elevator].push(floorNum); //queue the elevator for the next time you're going down.
+                    }
+                } else if (elevator.goingUpIndicator()) {
+                    floors_going_down[elevator].push(floorNum); //queue the elevator for the next time you're going up.
+                }
+                //window.alert("floor down " + floorNum + ": \nelevator: [" + elevator.destinationQueue + "], goingUp: [" + floors_going_up[elevator] + "], goingDown: [" + floors_going_down[elevator] + "]");
+            }
+            function diff(a,b) {
+                return a - b;
+            }
+            
             for (var i = 0; i < elevators.length; i++) {
                 elevator = elevators[i]
                 if (elevator.loadFactor() >= loadMax) {
                     for (var j = 0; j < elevator.destinationQueue.length; j++) {
-                        floorNum = elevator.destinationQueue[j];
-                        //if elevator is full before you visit floor in queue, don't visit that floor!
-                        if (elevator.destinationQueue.indexOf(floorNum) != -1) {
-                            while (elevator.destinationQueue.indexOf(floorNum) != -1) {
-                                elevator.destinationQueue.splice(elevator.destinationQueue.indexOf(floorNum), 1);
-                                elevator.checkDestinationQueue();
-                                window.alert(elevator.destinationQueue);
-                                j--;
+                        if (elevator.getPressedFloors().indexOf(elevator.destinationQueue[j]) == -1) {
+                            if (elevator.goingUpIndicator()) {
+                                up_pressed(elevators[parseInt(Math.random()*elevators.length,10)], elevator.destinationQueue[j]);
+                            } else {
+                                down_pressed(elevators[parseInt(Math.random()*elevators.length,10)], elevator.destinationQueue[j]);
                             }
-                            elevator.destinationQueue.sort();
-                            if (elevator.goingDownIndicator()) {
-                                elevator.destinationQueue.reverse();
-                            }
-                            elevator.checkDestinationQueue();
-
-                            //reassign the floor to another elevator
-                            //new_elevator = assign_elevator(floorNum, direction);
-                            //if (new_elevator == null) {
-                            //    new_elevator = elevators[parseInt(Math.random()*elevators.length, 10)];
-                            //}
-                            //if (new_elevator.goingUpIndicator()) {
-                            //    up_pressed(new_elevator, floorNum);
-                            //} else {
-                            //    down_pressed(new_elevator, floorNum);
-                            //}
                         }
                     }
+                    elevator.destinationQueue = elevator.getPressedFloors();
+                    elevator.destinationQueue.sort(diff);
+                    if (elevator.goingDownIndicator()) {
+                        elevator.destinationQueue.reverse();
+                    }
+                    elevator.checkDestinationQueue();
                 }
 
 
-            }*/
+            }
         }
 
 }
